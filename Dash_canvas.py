f57207eca96ab621c442065ee4a1fa5dd53041d7
Francesco <francesco@francescos-mac-mini.home>
2021-07-20 10:45:54 +0200

    test new username

--- Dash_canvas.py
+++ Dash_canvas.py
@@ -4,10 +4,17 @@ from dash_canvas.utils import parse_jsonstring
 from dash.dependencies import Input, Output, State
 from dash.exceptions import PreventUpdate
 
-import cairosvg
-from cairosvg import svg2png
+#import cairosvg
+#from cairosvg import svg2png
+
+#from svg.path import parse_path
+#from svgpathtools import wsvg
+from svgpathtools import parse_path, wsvg
+from svglib.svglib import svg2rlg
+from reportlab.graphics import renderPDF, renderPM
 
 import json
+import tempfile
 import dash_html_components as html
 import dash_core_components as dcc
 import dash
@@ -15,6 +22,7 @@ import pandas as pd
 import plotly
 import plotly.express as px
 import plotly.graph_objects as go
+
 
 
 app = dash.Dash(__name__)
@@ -50,20 +58,37 @@ def update_data(string):
     if string:
         print('string = ', string)
         data = json.loads(string)
-        print(type(data))
         print('data obj == ', data['objects'][0])
         print(type(data['objects'][0]))
-        print('path ======', data['objects'][0]['path'])
         svg_path = data['objects'][0]['path']
         print('svg_path == ', svg_path)
 
-        # use cairo to save the image ?
-        #svg2png(bytestring=svg_path, write_to='output_number.png')
-        # nope
+        inner_list = []
+        for block in svg_path:
+            inner_str = " ".join(map(str, block))
+            inner_list.append(inner_str)
 
-        # check this:
-        # https://pypi.org/project/svgpathtools/
-        # https://pypi.org/project/svg.path/
+        path_str = " ".join(inner_list)
+        print('path string = ', path_str)
+
+        path = parse_path(path_str)
+        print('parsed = ', path)
+        print("path is continuous? ", path.iscontinuous())
+        print("path is closed? ", path.isclosed())
+
+        # Save svg tempfile
+        t = tempfile.NamedTemporaryFile()
+        print(t.name)
+        wsvg(path, filename=t.name)
+        wsvg(path, filename='persistent.svg')
+        t.file.seek(0)
+        # do stuff
+
+        drawing = svg2rlg(t.name)
+        renderPM.drawToFile(drawing, "digit.png", fmt="PNG")
+        #
+        t.close()
+
     else:
         print('prevent')
         raise PreventUpdate
